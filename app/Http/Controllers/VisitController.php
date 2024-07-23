230942de277c756490d98606815ccb405a8d4a31
Babatunde Ogunbiyi <greyw4300@gmail.com>
2024-07-23 13:10:52 +0100

    update

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-//use App\Http\Requests\VisitorRequest;
 use App\Models\Host;
 use Inertia\Inertia;
 use App\Models\Visit;
 use Inertia\Response;
 use App\Models\Visitor;
 use Illuminate\Http\Request;
+use App\Mail\NewVisitNotification;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redirect;
+
 
 class VisitController extends Controller
 {
@@ -31,10 +33,10 @@ class VisitController extends Controller
         $visitsData = $visits->map(function ($visit) {
             return [
                 'visit_id' => $visit->visit_id,
-                'check_in_time' => $visit->check_in_time,
-                'check_out_time' => $visit->check_out_time,
                 'purpose' => $visit->purpose,
                 'status' => $visit->status,
+                'check_in_time' => $visit->created_at,
+                'check_out_time' => $visit->updated_at,
                 'visitor_name' => $visit->visitor->full_name,
                 'host_name' => $visit->host->host_name,
             ];
@@ -56,83 +58,58 @@ class VisitController extends Controller
         ]);
     }
  
-    // public function index(): Response
-    // {
-    //     $visits = Visit::with(['visitor', 'host'])->get();
-
-    //     $visitsData = $visits->map(function ($visit) {
-    //         return [
-    //             'visit_id' => $visit->visit_id,
-    //             'date' => $visit->date,
-    //             'check_in_time' => $visit->check_in_time,
-    //             'check_out_time' => $visit->check_out_time,
-    //             'purpose' => $visit->purpose,
-    //             'status' => $visit->status,
-    //             'visitor_name' => $visit->visitor->full_name,
-    //             'visitor_email' => $visit->visitor->email,
-    //             'visitor_company' => $visit->visitor->company,
-    //             'host_name' => $visit->host->host_name,
-    //             'host_department' => $visit->host->host_department,
-    //             'host_position' => $visit->host->host_position,
-    //         ];
-    //     });
-
-    //     return Inertia::render('Visits', [
-    //         'visits' => $visitsData,
-    //     ]);
-    // }
-
+  
 
     /**
      * Show the form for creating a new resource.
      */
     public function create(Request  $request)
     {
+       
 
-
+        return Inertia::render('Dashboard', [
+          'visits' => Visit::all(), // Adjust as needed
+            'allVisitors' => Visitor::all(),
+            'allHosts' => Host::all(),
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    // public function store(Request $request)
-
-
-
+    
     public function store(Request $request)
 {
     // Validate the request data
     $request->validate([
-        'visitor_name' => 'required|string|max:255',
-       
-        
+        'visitor_name' => 'required|string|max:255',   
         'host_name' => 'required|string|max:255',
-        
-       
-      
-        'check_in_time' => 'required',
-        'check_out_time' => 'required',
         'purpose' => 'required|string|max:255',
         'status' => 'required|string|max:255',
     ]);
 
-    // Fetch visitor_id from the visitors table
+ 
     $visitor = Visitor::where('full_name', $request->visitor_name)
         ->firstOrFail();
 
-    // Fetch host_id from the hosts table
+  
     $host = Host::where('host_name', $request->host_name)
         ->firstOrFail();
 
-    // Create the visit with the fetched ids
+
+        
     $visit = Visit::create([
         'visitor_id' => $visitor->id,
         'host_id' => $host->host_id,
-        'check_in_time' => $request->check_in_time,
-        'check_out_time' => $request->check_out_time,
         'purpose' => $request->purpose,
         'status' => $request->status,
     ]);
+
+    $hostEmail = $host->host_email;
+
+ 
+    Mail::to($hostEmail)->send(new NewVisitNotification($visit));
+    
 }
 
 
@@ -141,7 +118,7 @@ class VisitController extends Controller
      */
     public function show(string $id)
     {
-        $visitors = Visitor::all(); // Adjust according to your actual data fetching logic
+        $visitors = Visitor::all(); 
 
         return response()->json([
             'visitors' => $visitors
@@ -155,15 +132,6 @@ class VisitController extends Controller
     {
      
     }
-
-    //*
-
-//     public function search(Request $request)
-// {
-//     $query = $request->input('query');
-//     $visitors = Visitor::where('name', 'LIKE', "%{$query}%")->get(['id', 'full_name']); // Adjust the fields as needed
-//     return response()->json($visitors);
-// }
 
 
 
@@ -179,8 +147,6 @@ class VisitController extends Controller
             'visitor_name' => 'required|string|max:255',
             'host_name' => 'required|string|max:255',
         
-            'check_in_time' => 'required',
-            'check_out_time' => 'required',
             'purpose' => 'required|string|max:255',
             'status' => 'required|string|max:255',
         ]);
@@ -207,13 +173,9 @@ class VisitController extends Controller
 //     /**
 //      * Remove the specified resource from storage.
 //      */
-// //    public function destroy(string $id)
+// /
     public function destroy(Visit $visit)
     {
-
-        
-    
-        
-        $visit->delete();
+      $visit->delete();
     }
 }
--- public/build/assets/DeleteUserForm-DrHHV6Lm.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DeleteUserForm.vue_vue_type_script_setup_true_lang-B15Ekg2C.js";import"./app-D0CuMqhc.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./TextInput.vue_vue_type_script_setup_true_lang-qTjtIT2n.js";export{o as default};
